b6acc42e07625bcef3385c7b443e46e289ed414d
Amartya Singh <amartyasingh2002@gmail.com>
2023-02-15 23:09:25 +0530

    Codelab3 complete

---
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,31 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+
+class WellnessScreen {
+
+}
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
+    }
+}
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
